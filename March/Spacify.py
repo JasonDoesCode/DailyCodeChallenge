232c53b3c64f8ad87f4a941f28ae17e60b34cf9d
Jason Leonard <jasonleostudios@gmail.com>
2022-03-29 22:33:32 -0400

    Create Spacify.py

---
+++ March/Spacify.py
@@ -0,0 +1,16 @@
+# Spacify
+
+# CodeWars Link - https://www.codewars.com/kata/57f8ee485cae443c4d000127
+
+# Problem Statement
+# Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+# Example
+# Input: "Jason"
+# Output: "J a s o n"
+
+# My Solution
+# Learned that join() accepts strings, making it useful for string maniupluation
+def spacify(string):
+
+    return " ".join(string)
