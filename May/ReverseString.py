cd9ad66504865eb8a4e22637289a1cdc23b27220
Jason Leonard <jasonleostudios@gmail.com>
2022-05-31 13:37:19 -0400

    Create ReverseString.py

---
+++ May/ReverseString.py
@@ -0,0 +1,15 @@
+# Reversed Strings
+
+# Codewars Link - https://www.codewars.com/users/imJasonLeo/completed_solutions
+
+# Problem Statement
+# Complete the solution so that it reverses the string passed into it.
+
+# Examples
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+# My Solution
+# Make use of Slice notation and step backwards through the string
+def solution(str):
+    return str[::-1]
