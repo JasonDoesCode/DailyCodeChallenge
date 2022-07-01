f1bac9e838ed800576ae52210f5d45c88275be5c
Jason <jasonleostudios@gmail.com>
2022-07-01 16:50:50 -0400

    Create OppositeNumber.js

---
+++ July/OppositeNumber.js
@@ -0,0 +1,21 @@
+// Opposite Number (JavaScript)
+
+// CodeWars Link - https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+// Problem Statement
+// Given an integer or a floating-point number, find its opposite.
+
+// Example
+// 1 -> -1
+// -34 -> 34
+
+// My Solution
+function opposite(number) {
+    return number * -1
+}
+
+// Another Solution
+// Doesn't need to multiply the number
+function opposite(number) {
+    return (-number)
+}
