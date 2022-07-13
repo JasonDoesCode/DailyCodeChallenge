59e8edf93d60ef169c15cf101282994f629a00e9
Jason <jasonleostudios@gmail.com>
2022-07-13 13:57:45 -0400

    Create DisemvowelTrolls.js

---
+++ July/DisemvowelTrolls.js
@@ -0,0 +1,19 @@
+// Vowel Count
+
+//CodeWars Link - https://www.codewars.com/kata/52fba66badcd10859f00097e
+
+// Problem Statement
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+
+// My Solution
+function disemvowel(str) {
+    return str.replace(/[aeiouAEIOU]/g, "");
+}
+
+// Better Solution
+// uses gi which is case insensitive
+function disemvowel(str) {
+    return str.replace(/[aeiou]/gi, "");
+}
