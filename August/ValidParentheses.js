0be0a90fc171caf9d88a92c4c9234a96580e8c8b
Jason <jasonleostudios@gmail.com>
2022-08-20 16:28:29 -0400

    Optimized original solution for Valid Parentheses

--- August/ValidParentheses.js
+++ August/ValidParentheses.js
@@ -23,7 +23,7 @@
 // If they match, continue iterating, if they don't match return false
 // If the we iterate through the whole string but the stack isn't empty, return false ( e.x scenario: '((' or '[{' )
 var isValid = function (s) {
-    //Use a stack -> push left chars on to the stack as they appear -> pop stack when right char appears and see if the char matches
+    //Use a stack -> push left chars on to the stack as they appear -> pop stack when right char appears and see if the char        matches
 
     const brackets = []
 
@@ -35,9 +35,9 @@ var isValid = function (s) {
 
     for (let i = 0; i < s.length; i++) {
         if (pairs.hasOwnProperty(s[i])) {
-            brackets.unshift(s[i])
+            brackets.push(s[i])
         } else {
-            if (pairs[brackets.shift()] !== s[i]) {
+            if (pairs[brackets.pop()] !== s[i]) {
                 return false
             }
         }
